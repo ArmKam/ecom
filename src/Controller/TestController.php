bacd92bfb0542df579a1a01969b74305f03e964b
ArmKam <kamoep.84@gmail.com>
2021-11-12 21:45:15 +0100

    require annotation and use annotation to handle root, comment out routes.yaml file

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,11 +2,24 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
+
 class TestController
 {
+    /**
+     * @Route("/", name="index")
+     */
     public function index()
     {
         dump("Ça fucntion");
         die();
+    }
+
+    /**
+     * @Route("/test", name ="test");
+     */
+    public function test()
+    {
+        dd("Hello from test");
     }
 }
