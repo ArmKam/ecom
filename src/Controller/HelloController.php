2f51490d1ead521f56e6c791fa30e003d44e3cfa
ArmKam <kamoep.84@gmail.com>
2021-11-17 17:30:04 +0100

    Setup Doctrine and our first entity

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -10,43 +10,77 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HelloController
+class HelloController extends AbstractController
 {
 
-    protected $calculator;
 
-    public function __construct(Calculator $calculator)
-    {
-        $this->calculator = $calculator;
-    }
+
+    // protected $twig;
+
+    // public function __construct(Environment $twig)
+    // {
+    //     $this->twig = $twig;
+    // }
+    // protected $calculator;
+
+    // public function __construct(Calculator $calculator)
+    // {
+    //     $this->calculator = $calculator;
+    // }
     // protected $logger;
     // public function __construct(LoggerInterface $logger)
     // {
 
     //     $this->logger = $logger;
     // }
+    // /**
+    //  * @Route("/hello/{prenom<[a-zA-Z]+>?World}",  name="hello")
+    //  */
+    // public function hello(LoggerInterface $logger, Slugify $slugify, $prenom, Environment $twig, Detector $detecteur)
+    // {
+    //     dump($detecteur->detect(101));
+    //     dump($detecteur->detect(10));
+    //     // $slugify  = new Slugify();
+    //     dump($twig);
+    //     //dump($slugify->slugify("Hello Worlds"));
+    //     // dump($request);
+    //     // $this->logger->info("This log created by $prenom");
+    //     //$logger->info("This log created by $prenom");
+    //     $tva = $this->calculator->calcul(100);
+    //     dump($tva);
+    //     $logger->info("an operation is made $tva");
+    //     return new Response("Hello $prenom");
+    // }
+
     /**
      * @Route("/hello/{prenom<[a-zA-Z]+>?World}",  name="hello")
      */
-    public function hello(LoggerInterface $logger, Slugify $slugify, $prenom, Environment $twig, Detector $detecteur)
+    public function hello($prenom)
     {
 
-
-        dump($detecteur->detect(101));
-        dump($detecteur->detect(10));
-
-
-        // $slugify  = new Slugify();
-
-        dump($twig);
-        //dump($slugify->slugify("Hello Worlds"));
-        // dump($request);
-        // $this->logger->info("This log created by $prenom");
-        //$logger->info("This log created by $prenom");
-        $tva = $this->calculator->calcul(100);
-        dump($tva);
-        $logger->info("an operation is made $tva");
-        return new Response("Hello $prenom");
+        return $this->render("hello.html.twig", [
+            "prenom" => $prenom
+            // ,
+            // "formateur1" => ["prenom" => "Kamo",  "nom" => "Yepremyan"],
+            // "formateur2" => ["prenom" => "Lior", "nom" => "Chamla"]
+        ]);
     }
+
+    /**
+     * @Route("/exemple", name="exemple")
+     */
+    public function exemple()
+    {
+        return   $this->render("exemple.html.twig", [
+            "age" => 33
+        ]);
+    }
+
+    // protected function render(string $path, array $variable = [])
+    // {
+    //     $html = $this->twig->render($path, $variable);
+    //     return new Response($html);
+    // }
 }
